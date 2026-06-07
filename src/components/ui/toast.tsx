adb2ae643e9332cd4e885031e7bf52d97311db7b
GILTON <samuelmarks222@gmail.com>
2026-06-07 16:04:09 +0300

    Toast 200px from top

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -14,7 +14,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed left-0 top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:left-[50px] sm:right-auto sm:top-[100px] sm:flex-col md:max-w-[420px]",
+      "fixed left-0 top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:left-[50px] sm:right-auto sm:top-[200px] sm:flex-col md:max-w-[420px]",
       className,
     )}
     {...props}
